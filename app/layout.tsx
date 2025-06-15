2f05627787ecf0c80fbce03e39f39189f06bb51d
Ilya Duchamp <ilya@meekot.com>
2025-06-15 20:53:45 +0200

    Update favicon assets position

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,36 +20,36 @@ export const metadata: Metadata = {
       {
         rel: "icon",
         type: "image/ico",
-        url: "/favicon/favicon.ico",
+        url: "/favicon.ico",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "16x16",
-        url: "/favicon/favicon-16x16.png",
+        url: "/favicon-16x16.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "32x32",
-        url: "/favicon/favicon-32x32.png",
+        url: "/favicon-32x32.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "192x192",
-        url: "/favicon/android-chrome-192x192.png",
+        url: "/android-chrome-192x192.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "512x512",
-        url: "/favicon/android-chrome-512x512.png",
+        url: "/android-chrome-512x512.png",
       },
     ],
   },
 
-  manifest: "/favicon/site.webmanifest",
+  manifest: "/site.webmanifest",
 }
 
 export default function RootLayout({
